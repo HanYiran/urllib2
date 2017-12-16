ec21ce7e22a9755112f56eed35800c31fa6da5e9
HanYiran <815520326@qq.com>
2017-12-16 20:50:50 +0800

    爬取百度首页(构建Request对象)并写入到文件中

--- urllibpractice.py
+++ urllibpractice.py
@@ -6,13 +6,16 @@ def load_baidu():
 
     url = 'http://www.baidu.com'
 
-    response = urllib2.urlopen(url)
+    #response = urllib2.urlopen(url)
+    #可以构建请求对象之后,Request 是大写
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
 
     html = response.read()
 
     return html
 def write_file(html):
-    with open('baidu01.html','w') as f :
+    with open('baidu02.html','w') as f :
         f.write(html)
 
 if __name__ == '__main__':
