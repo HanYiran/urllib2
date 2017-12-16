bcad6ef3303e1b9053049b8de40564593fe85300
HanYiran <815520326@qq.com>
2017-12-16 21:28:57 +0800

    修改user-agent 隐藏身份(另外一种方法)

--- addHeaderUseragent.py
+++ addHeaderUseragent.py
@@ -4,12 +4,15 @@ import urllib2
 def load_baidu():
 
     url = 'http://www.baidu.com'
-    request =urllib2.Request(url)
 
-    #修改 请求头 隐藏 user-agent
-    request.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko")
-    request.add_header("Connection", "keep-alive")
+    # 修改 请求头 隐藏 user-agent
+    # request =urllib2.Request(url)
+    # request.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko")
+    # request.add_header("Connection", "keep-alive")
 
+    #第二种方式
+    header = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3"}
+    request = urllib2.Request(url,headers=header)
     #先请求 response 在 查看请求头
     response = urllib2.urlopen(request)
 
